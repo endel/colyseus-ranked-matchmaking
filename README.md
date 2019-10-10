2a2c79a6df85a212c0bcbaf5572f073e0a03edce
Endel Dreyer <endel.dreyer@gmail.com>
2019-10-10 15:58:19 -0300

    refactoring redistributeGroups() method. add unit tests

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This is an example on how to achieve Ranked Matchmaking within Colyseus.
 
 This may be included as part of the core in the future. Feedback and more use-cases are necessary to ensure the most common ones are covered by this implementation.
 
+## How to achieve Ranked Matchmaking
+
+
+
 ## Usage
 
 TODO
--- RankedLobbyRoom.ts
+++ RankedLobbyRoom.ts
@@ -1,22 +1,26 @@
 import { Room, Client } from "colyseus";
 
 interface MatchmakingGroup {
-  clients: Client[]
+  averageRank: number;
+  clients: ClientStat[],
   locked?: boolean;
 }
 
-interface ClientStats {
-  rank: number;
+interface ClientStat {
+  client: Client;
+  waitingTime: number;
   group?: MatchmakingGroup;
+  rank: number;
+  distance?: number; // used for sorting
 }
 
 export class RankedLobbyRoom extends Room {
   groups: MatchmakingGroup[] = [];
 
   maxWaitingTime = 15;
-  numClientsToMatch = 2;
+  numClientsToMatch = 4; // 2
 
-  stats: { [sessionId: string]: ClientStats } = {};
+  stats: ClientStat[] = [];
 
   onCreate(options: any) {
     if (options.maxWaitingTime) {
@@ -26,17 +30,16 @@ export class RankedLobbyRoom extends Room {
     if (options.numClientsToMatch) {
       this.numClientsToMatch = options.numClientsToMatch;
     }
+
+    // this.setSimulationInterval(() => this.redistributeGroups(), 5000);
   }
 
   onJoin(client: Client, options: any) {
-    const stat: ClientStats = {
+    this.stats.push({
+      client: client,
       rank: options.rank,
-      group: this.groups
-        .filter(g => !g.locked)
-        .sort((a, b) => (
-          this.getAcceptanceRatioInGroup(client, a) - this.getAcceptanceRatioInGroup(client, b)
-        ))[0]
-    };
+      waitingTime: 0
+    });
 
     /**
      * TODO:
@@ -50,33 +53,98 @@ export class RankedLobbyRoom extends Room {
      * -
      */
 
-    if (!stat.group) {
-      stat.group = { clients: [client] };
-      this.groups.push(stat.group);
-    }
-
-    this.stats[client.sessionId] = stat;
   }
 
   onMessage(client: Client, message: any) {
   }
 
-  getAcceptanceRatioInGroup (client: Client, group: MatchmakingGroup) {
-    const rank = this.stats[client.sessionId].rank;
+  createGroup() {
+    let group: MatchmakingGroup = { clients: [], averageRank: 0 };
+    this.groups.push(group);
+    return group;
+  }
 
-    const avg = group.clients.reduce<number>((previous, current) => {
-      previous += this.stats[current.sessionId].rank;
+  redistributeGroups() {
+    /**
+     * Keep only `locked` groups.
+     */
+    this.groups = this.groups.filter(group => group.locked);
+
+    let highestRank = 0;
+
+    const stats = this.stats
+      .filter(stat => {
+        if (stat.rank > highestRank) { highestRank = stat.rank; }
+        return !stat.group || !stat.group.locked
+      })
+      .sort((a, b) => a.rank - b.rank);
+
+    let currentGroup: MatchmakingGroup = this.createGroup();
+    let totalRank = 0;
+
+    for (let i = 0, l = stats.length; i < l; i++) {
+      const stat = stats[i];
+
+      if (currentGroup.clients.length === this.numClientsToMatch) {
+        currentGroup = this.createGroup();
+        totalRank = 0;
+      }
+
+      if (currentGroup.averageRank > 0) {
+        const diff = Math.abs(stat.rank - currentGroup.averageRank);
+        const diffRatio = (diff / currentGroup.averageRank);
+
+        if (diffRatio > 2) {
+          currentGroup = this.createGroup();
+          totalRank = 0;
+        }
+      }
+
+      stat.group = currentGroup;
+      currentGroup.clients.push(stat);
+
+      totalRank += stat.rank;
+
+      currentGroup.averageRank = totalRank / currentGroup.clients.length;
+    }
+  }
+
+  getAcceptanceRatePerGroups (stat: ClientStat) {
+    return this.groups
+      .filter(g => !g.locked)
+      .map((group) => ({
+        group: group,
+        ...this.getAcceptanceRatioInGroup(stat, group)
+      }))
+      .sort((a, b) => a.acceptanceRatio - b.acceptanceRatio);
+  }
+
+  getAcceptanceRatioInGroup (stat: ClientStat, group: MatchmakingGroup) {
+    const groupLength = group.clients.length + 1;
+    const rank = stat.rank;
+
+    const averageRank = (rank + group.clients.reduce<number>((previous, current) => {
+      previous += current.rank;
       return previous;
-    }, 0) / group.clients.length;
+    }, 0)) / groupLength;
 
-    const diff = Math.abs(rank - avg);
+    const diff = Math.abs(rank - averageRank);
 
-    return (diff / avg);
-
+    return {
+      averageRank,
+      /**
+       * Acceptance ratio:
+       *     1 = same rank
+       *     <1 = player's rank is higher
+       *     >1 = player's rank is lower
+       */
+      acceptanceRatio: (diff / averageRank),
+    };
   }
 
   onLeave(client: Client, consented: boolean) {
-    delete this.stats[client.sessionId];
+    const index = this.stats.findIndex(stat => stat.client === client);
+    this.stats.splice(index, 1);
   }
 
   onDispose() {
